7fe0a21f023cb013f96bae3a2dc27b5e3449c93b
manuel <delverme@equilibretechnologies.com>
2023-07-04 22:18:09 +0200

    [CLUSTER] no_id

--- text_only_baseline.py
+++ text_only_baseline.py
@@ -175,17 +175,22 @@ def main(hyper: omegaconf.DictConfig):
 
 def deploy(hostname):
     import experiment_buddy
+    os.environ['BUDDY_DEBUG_DEPLOYMENT'] = "1"
+    experiment_buddy.deploy(url="hydra://mila")
     git_repo = git.Repo(search_parent_directories=True)
     experiment_id = experiment_buddy.ask_experiment_id(hostname, sweep="")
     hash_commit = experiment_buddy.git_sync(experiment_id, git_repo)
     url = urllib.parse.urlparse(f"ssh://{hostname}")
     executor = experiment_buddy.executors.SSHExecutor(url=url)
     executor.setup_remote(extra_slurm_header=None, working_dir=git_repo.working_dir)
-    experiment_folder = executor.remote_checkout(git_url=git_repo.remotes.origin.url, hash_commit=hash_commit)
+
     assert __name__ == "__main__"
     entrypoint = os.path.basename(__file__)
-    with executor.ssh_session.cd(experiment_folder):
-        executor.run(f"source /etc/profile && source ~/venv/bin/activate && python3 {entrypoint} --multirun hydra/launcher=submitit_slurm")
+    with executor.ssh_session.cd(executor.scripts_folder):
+        executor.run(f"python hydra/run_experiment.sh {git_repo.remotes.origin.url} {entrypoint} {hash_commit}")
+
+    # executor.put(os.path.join(git_repo.working_dir, "run_experiment.sh"),
+    #              os.path.join(executor.scripts_folder, "run_experiment.sh"))
 
 
 if __name__ == '__main__':
