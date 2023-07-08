1e856c931d342e282cd1a991a745936c9a6766cc
manuel <delverme@equilibretechnologies.com>
2023-07-08 16:51:57 +0200

    [CLUSTER] parse_all_layers

--- hyper.py
+++ hyper.py
@@ -2,9 +2,10 @@ TOKEN_CONTEXT_LEN = 18
 POINTS_IN_MOTOR_SEQUENCE = 64
 IMAGE_SIDE = 28
 
-learning_rate = 1e-3  # 3e-5
+learning_rate = 0.0006247650066162154
+# learning_rate = 1e-3  # 3e-5
 batch_size = 32
 training_hours = 1
 
-num_layers = 1
+num_layers = 0
 hidden_size = 128
