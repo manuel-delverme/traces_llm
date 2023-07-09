50fe7ba614ef53d2b6ac2c4929c0b973ccd7f8ba
manuel <delverme@equilibretechnologies.com>
2023-07-09 14:47:25 +0200

    [CLUSTER] profile

--- hyper.py
+++ hyper.py
@@ -2,6 +2,8 @@ TOKEN_CONTEXT_LEN = 18
 POINTS_IN_MOTOR_SEQUENCE = 64
 IMAGE_SIDE = 28
 
+early_stopping_patience = 32
+
 learning_rate = 0.0006247650066162154
 # learning_rate = 1e-3  # 3e-5
 batch_size = 32
